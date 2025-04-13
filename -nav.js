17d5db8976f54967ded7279349deb63b393510c3
butko <abutko@gmail.com>
2025-04-13 18:36:55 +0300

    чистка

--- -nav.js
+++
@@ -1,4 +0,0 @@
-var list=2;
-var f="http://ab-lib.narod.ru/df/"
-var nav1=["df_text.htm",f+"df_link.htm",f+"index.htm","../index.htm"];
-var nav2=["Тексты","ДФ-овские ссылки","День Физика","А.Б.иблиотека"];
--- -panel.js
+++
@@ -1,7 +0,0 @@
-document.write("<td width=30% valign=top bgcolor=#99CCCC><h3>Поиск:</h3><center><form action=http://narod.yandex.ru/cgi-bin/yandsearch><input type=hidden name=user value=ab-lib><input name=text><p><input type=submit value=Искать!><input type=hidden name=where value=2></form></center>");
-
-//
-document.write("<hr width=150><h3>Общение:</h3><small><br><a href=http://www.guestbook.ru/book.cgi?user=butko&action=sign>Оставить запись в гостевой книге</a><br><a href=http://www.guestbook.ru/book.cgi?user=butko&action=show>Посмотреть гостевую книгу</a><br><a href=mailto:abutko@gmail.com?subject=web_site>Написать письмо владельцу сайта</a><br><a href=http://ab-lib.narod.ru/phpbb/>Форум сайта</a><br><a href=http://subscribe.ru/catalog/lit.book.library.ablib>Рассылка сайта</a><br><a href=http://www.livejournal.com/users/butko/>Живой журнал</a></small>");
-
-//
-document.write("</tr><tr><td colspan=3 valign=bottom bgcolor=#99CCCC><small><p align=right><a href=http://ab-lib.narod.ru>http://ab-lib.narod.ru</a><br><nobr>web-master &copy; <a href=http://ab-lib.narod.ru/butko/ab_site.htm>Бутко А.В.</a></small></nobr></tr></table>");
